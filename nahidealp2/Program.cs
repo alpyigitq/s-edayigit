51d9754a7b9dc10e451d4f53f82aba2974aa6f97
alpyigitq <alp.yigitq@gmail.com>
2025-06-09 15:46:56 +0300

    nahidealp2

---
+++ nahidealp2/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nahidealp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region If Else
+
+            //Console.Write("Lütfen şifreyi giriniz: ");
+            //string password;
+            //password = Console.ReadLine();
+            //if (password == "abcd")
+            //{
+            //    Console.WriteLine("Şifre Doğru");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Şifre Yanlış");
+            //}
+
+            //string capital, country;
+            //Console.Write("Başkenti Giriniz: ");
+            //capital=Console.ReadLine();
+
+            //Console.Write("Ülkeyi Giriniz: ");
+            //country=Console.ReadLine();
+
+            //if (capital=="ankara" & country=="türkiye")
+            //{
+            //    Console.Write("Veriler Doğrulandı");
+            //}
+            //else
+            //{
+            //    Console.Write("hatalı bilgi");
+            //}
+
+            //int number;
+            //Console.Write("sayıyı giriniz: ");
+            //number = int.Parse(Console.ReadLine());
+            //if (number == 5) 
+            //{
+            //    Console.WriteLine("Sayı Doğru ");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Sayı hatalı ");
+            //}
+
+            //int exam1, exam2, average;
+            //string result = "Hata!"; 
+
+            //Console.Write("Sınav1: ");
+            //exam1 = int.Parse(Console.ReadLine());
+            //Console.Write("Sınav2: ");
+            //exam2 = int.Parse(Console.ReadLine());
+
+            //average = (exam1 + exam2) / 2;
+            //Console.WriteLine("Sınavların Ortalaması: " + average);
+
+            //if (average > 0 & average <= 50)
+            //{
+            //    result = "Sonuç Vasat";
+            //}
+            //if (average > 50 & average <= 70)
+            //{
+            //    result = "Sonuç Orta";
+            //}
+            //if (average > 70 & average <= 84)
+            //{
+            //    result = "Sonuç İyi";
+            //}
+            //if (average > 84 & average <=100)
+            //{
+            //    result = "Sonuç Çok İyi";
+            //}
+
+            //Console.WriteLine(result);
+
+            #endregion
+
+            Console.Read();
+        }
+    }
+}
